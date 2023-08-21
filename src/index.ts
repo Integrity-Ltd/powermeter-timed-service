e84bb1ec1fa3b2f4bbea7ef32260dbe32f9ec75f
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-08-21 15:51:44 +0200

    copy config file to the path if not exists

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ import { yearlyProcess } from './yearlydeclarations';
 
 dotenv.config({ path: path.resolve(__dirname, `../${process.env.NODE_ENV ? process.env.NODE_ENV as string : ""}.env`) });
 
-if (process.env.NODE_ENV === "docker" && !fs.existsSync(path.join(process.env.WORKDIR as string, "config.sqlite"))) {
-    fs.copyFileSync(path.resolve(__dirname, "../config.sqlite"), path.join(process.env.WORKDIR as string, "config.sqlite"));
+if (!fs.existsSync(process.env.CONFIG_DB_FILE_NAME as string)) {
+    fs.copyFileSync(path.resolve(__dirname, "../config.sqlite"), process.env.CONFIG_DB_FILE_NAME as string);
 }
 
 cron.schedule(process.env.YEARLY_CRONTAB as string, () => {
